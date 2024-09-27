ea888241dca5a29c2be7b2335274e07ef827b8f6
JoaoVitorAlvesR <joaovitor.alvesr@unesc.net>
2024-09-26 23:04:39 -0300

    reponsive adjusts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { PixelGrid } from "@/components";
 export default function Home() {
   return (
     <>
-      <h1 className="flex flex-row m-4 items-center justify-center">
+      <h1 className="flex flex-row m-20 items-center justify-center">
         Clique nos quadrados para marcar
       </h1>
       <div className="flex flex-row m-4 items-center justify-center">
